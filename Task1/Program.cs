fd3ceed0a7334b0720bf6faa2847b5368ba25d26
Bibek Budhathoki <ashishbudhathoki354@gmail.com>
2025-11-12 09:34:03 +0545

    orkhop3

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Workshop;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operators op = new Operators();
+            op.Add(12, 5);
+            op.Subtract(30, 15);
+            op.Multiply(12, 4);
+            op.Divide(12, 2);
+            op.OddEvenFinder(7);
+        }
+    }
+}
